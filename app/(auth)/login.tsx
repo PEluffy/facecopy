ce3a2d37c90631c9bc680a76f47be85168929a93
Prashannakafle4444 <kafleprakrit@gmail.com>
2025-03-08 00:11:57 +0545

    auth login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { styles } from "@/styles/auth.styles";
 import { Ionicons } from "@expo/vector-icons";
@@ -17,6 +17,29 @@ export default function login() {
         </View>
         <Text style={styles.appName}>facecopy</Text>
         <Text style={styles.tagline}>don't miss anything</Text>
+      </View>
+      {/* ILLUSTRATION */}
+      <View style={styles.illustrationContainer}>
+        <Image
+          style={styles.illustration}
+          source={require("../../assets/images/auth.png")}
+        />
+      </View>
+      {/* Login Section*/}
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          onPress={() => console.log("continue with google")}
+          activeOpacity={0.9}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>
+          By continuing, you aggree to our Terms and Privacy policy
+        </Text>
       </View>
     </View>
   );
